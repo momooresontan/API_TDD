cd811a5a28ce9cc732ac75c4de93215f5d1100a8
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-06-16 01:52:08 +0100

    test file modified(get one & 404 test done)

--- app.test.js
+++ app.test.js
@@ -19,9 +19,24 @@ describe("Todos API", () => {
       });
   });
 
-  it("GET /todos/:id --> specific todo by ID", () => {});
+  it("GET /todos/:id --> specific todo by ID", () => {
+    return request(app)
+      .get("/todos/1")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toEqual(
+          expect.objectContaining({
+            name: expect.any(String),
+            completed: expect.any(Boolean),
+          })
+        );
+      });
+  });
 
-  it("GET /todos/:id --> 404 if not found", () => {});
+  it("GET /todos/:id --> 404 if not found", () => {
+    return request(app).get("/todos/99999").expect(404);
+  });
 
   it("POST /todos --> created todo", () => {});
 
