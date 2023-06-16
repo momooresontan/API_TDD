73cef1cb79d729a2239a4ec963d038628bec0838
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-06-16 01:30:51 +0100

    test file modified(post, validate, patch and delete added)

--- app.test.js
+++ app.test.js
@@ -2,7 +2,19 @@ const request = require("supertest");
 const app = require("./app");
 
 describe("Todos API", () => {
-  it("GET /todos --> array todos", () => {});
+  it("GET /todos --> array todos", () => {
+    return request(app).get("/todos");
+  });
 
-  it("GET /todos/:id --> specific todo by id", () => {});
+  it("GET /todos/:id --> specific todo by ID", () => {});
+
+  it("GET /todos/:id --> 404 if not found", () => {});
+
+  it("POST /todos --> created todo", () => {});
+
+  it("GET /todos --> validates request body", () => {});
+
+  it("PATCH /todos/:id --> updated todo", () => {});
+
+  it("DELETE /todos/:id --> 204 no content", () => {});
 });
