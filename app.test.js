f7b9583fe7ffe15b0ab92d00248ceffb421e19ca
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-06-16 01:41:27 +0100

    test file modified(get all test done)

--- app.test.js
+++ app.test.js
@@ -3,7 +3,20 @@ const app = require("./app");
 
 describe("Todos API", () => {
   it("GET /todos --> array todos", () => {
-    return request(app).get("/todos");
+    return request(app)
+      .get("/todos")
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({
+              name: expect.any(String),
+              completed: expect.any(Boolean),
+            }),
+          ])
+        );
+      });
   });
 
   it("GET /todos/:id --> specific todo by ID", () => {});
@@ -15,6 +28,8 @@ describe("Todos API", () => {
   it("GET /todos --> validates request body", () => {});
 
   it("PATCH /todos/:id --> updated todo", () => {});
+
+  it("PATCH /todos/:id --> 404 if not found", () => {});
 
   it("DELETE /todos/:id --> 204 no content", () => {});
 });
