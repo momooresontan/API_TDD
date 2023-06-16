d4cb170996e67b55d05dd116d9d2d34072d9cfe1
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-06-16 04:02:00 +0100

    test file modified & delete route created

--- routes/todosRoute.js
+++ routes/todosRoute.js
@@ -36,14 +36,28 @@ router.post("/", function (req, res, next) {
   res.status(201).json(newTodo);
 });
 
-router.patch("/:id", function (req, res) {
+router.patch("/:id", function (req, res, next) {
   let foundTodo = todos.find((todo) => todo.id === Number(req.params.id));
+
+  if (!foundTodo) {
+    return next(createError(404, "Not Found"));
+  }
+
+  if (typeof req.body.name !== "string") {
+    return next(createError(422, "Validation Error"));
+  }
 
   foundTodo["name"] = req.body.name;
 
   const updatedTodo = foundTodo;
 
   res.status(200).json(updatedTodo);
+});
+
+router.delete("/:id", function (req, res, next) {
+  let foundTodo = todos.find((todo) => todo.id === Number(req.params.id));
+
+  const index = todos.ind;
 });
 
 module.exports = router;
